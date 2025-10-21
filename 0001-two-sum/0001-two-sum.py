6272f7d6b148af797925062809755b47e16ca800
emagara <104788365+EnockMagara@users.noreply.github.com>
2025-10-21 22:39:15 +0400

    Time: 0 ms (100%), Space: 13 MB (90.38%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,17 @@
 class Solution(object):
     def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
         num_map = {}
-        for i, num in enumerate(nums):
-            complement = target - num
+        for i, current in enumerate(nums):
+            complement = target - current
+
             if complement in num_map:
                 return [num_map[complement], i]
-            num_map[num] = i
+
+            num_map[current] = i
+        
